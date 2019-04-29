5cf67aecc0f50d19f823cdbfc41a3950a3d8554c
Joe Bryant <Joe.Bryant@Me.com>
2019-04-29 14:07:07 -0700

    Added array func

--- stringParsers.go
+++ stringParsers.go
@@ -1,19 +1,16 @@
 package stringParsers
 
-import (
-	"log"
-	"strings"
-)
-
 func SplitOptions(s string) []string {
 	t := s
 	tok := ""
 	as := []string{}
-	for ; string(t) != ""; tok, t = quoted(t) {
+	for tok, t = quoted(t); string(t) != ""; tok, t = quoted(t) {
 		as = append(as, tok)
 	}
-	log.Println(as)
-	return strings.Split(s, " ")
+	if tok != "" {
+		as = append(as, tok)
+	}
+	return as
 }
 
 func Bracket(s string) string {
@@ -24,6 +21,7 @@ func BracketAll(s []string) []string {
 	as := []string{}
 	for _, t := range s {
 		as = append(as, Bracket(t))
+		//time.Sleep(5*time.Second)
 	}
 	return as
 }
@@ -31,31 +29,50 @@ func BracketAll(s []string) []string {
 func quoted(s string) (string, string) {
 	at := 0
 	token := ""
+	dlim := ""
 	// skip leading blanks
-	for ; string(s[at]) != " "; at++ {
+	for ; string(s[at]) == " "; at++ {
+		//log.Println("Stripping leading blanks:",Bracket(string(s)))
 	}
 	// if first char is quote, consume until ending quote, else consume until blank
-	if string(s[at]) == "\"" {
+	//log.Println("First char:",string(s[at]))
+	if string(s[at]) == "\"" || string(s[at]) == "'" {
+		dlim = string(s[at])
 		at++
 		esc := false
-		for _, t := range s[at:] {
+		//log.Println("a",dlim)
+		for i, t := range s[at:] {
+			//log.Println("b")
 			if esc {
+				//log.Println("c")
 				esc = false
 				token += string(t)
-			} else if string(t) == "\"" {
-				return token, s[at:]
+			} else if string(t) == dlim {
+				//log.Println("d",token,at+i)
+				return token, s[at+i+1:]
 			} else {
+				//log.Println("e")
 				if string(t) == "\\" {
+					//log.Println("f")
 					esc = true
 				} else {
+					//log.Println("g",token)
 					token += string(t)
 				}
 			}
+			//log.Println("In a")
+			//time.Sleep(5*time.Second)
 		}
 
 	} else {
-		for ; string(s[at]) != " " && at < len(s); at++ {
-
+		//log.Println("h",at)
+		for token = ""; at < len(s); at++ {
+			//log.Println("In b")
+			//time.Sleep(5*time.Second)
+			if string(s[at]) == " " {
+				return token, s[at:]
+			}
+			token += string(s[at])
 		}
 	}
 	return token, s[at:]
