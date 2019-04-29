b0ac549b957d413f8ae2a410936bf15fa06e0132
Joe Bryant <Joe.Bryant@Me.com>
2019-04-29 10:44:23 -0700

    Added array func

--- stringParsers.go
+++ stringParsers.go
@@ -1,8 +1,18 @@
 package stringParsers
 
-import "strings"
+import (
+	"log"
+	"strings"
+)
 
 func SplitOptions(s string) []string {
+	t := s
+	tok := ""
+	as := []string{}
+	for ; string(t) != ""; tok, t = quoted(t) {
+		as = append(as, tok)
+	}
+	log.Println(as)
 	return strings.Split(s, " ")
 }
 
@@ -16,4 +26,37 @@ func BracketAll(s []string) []string {
 		as = append(as, Bracket(t))
 	}
 	return as
+}
+
+func quoted(s string) (string, string) {
+	at := 0
+	token := ""
+	// skip leading blanks
+	for ; string(s[at]) != " "; at++ {
+	}
+	// if first char is quote, consume until ending quote, else consume until blank
+	if string(s[at]) == "\"" {
+		at++
+		esc := false
+		for _, t := range s[at:] {
+			if esc {
+				esc = false
+				token += string(t)
+			} else if string(t) == "\"" {
+				return token, s[at:]
+			} else {
+				if string(t) == "\\" {
+					esc = true
+				} else {
+					token += string(t)
+				}
+			}
+		}
+
+	} else {
+		for ; string(s[at]) != " " && at < len(s); at++ {
+
+		}
+	}
+	return token, s[at:]
 }
